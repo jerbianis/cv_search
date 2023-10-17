3b2770bd3b9599235b868614b820eba503a2413e
jerbianis <anisjerbi2@gmail.com>
2023-10-17 09:34:28 +0100

    version 3

--- pdf_to_txt.py
+++ pdf_to_txt.py
@@ -5,8 +5,7 @@ import os
 
 
 def extract_text_from_pdf(pdf_path):
-    #use this command to know the path (which pdftotext)
-    text = textract.parsers.process(pdf_path,input_encoding='Latin-1')
+    text = textract.parsers.process(pdf_path)
     return text
 
 pdf_folder = './CVs'
